de05078fb623643a4423b67593cb9f1380873e21
Piyush Sharma <111640586+Piyushsharma2004@users.noreply.github.com>
2023-08-11 17:56:22 +0530

    Add files via upload

---
+++ Mode_S/video-slide.js
@@ -0,0 +1,33 @@
+var carousel = document.getElementById("carousel-1");
+var slides = carousel.querySelectorAll(".slide-1");
+var currentSlide = 0;
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  slides[currentSlide].style.display = "block";
+  slides[currentSlide - 1].style.display = "none";
+}
+
+setInterval(nextSlide, 100000);
+
+var manualButton = document.getElementById("manual");
+var autoplayButton = document.getElementById("autoplay");
+
+manualButton.addEventListener("click", function() {
+  if (autoplayButton.checked) {
+    autoplayButton.checked = false;
+  }
+});
+
+autoplayButton.addEventListener("click", function() {
+  if (manualButton.checked) {
+    manualButton.checked = false;
+  }
+});
+
+
+//
